2732a3d33b59ab898fa72873c093a3bc842e18c8
dericklopis <stanforever08@gmail.com>
2022-11-15 21:08:23 +0530

    Added: No play, Ladder or Snake

--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -10,12 +10,40 @@ namespace SnakesAndLadders
     {
         static void Main(string[] args)
         {
-            int FirstPosition=0;
-            int PlayerPosition=FirstPosition;
+            const int FirstPosition =0;
+            int position = 0;
+            
             Console.WriteLine("First player is starting with position " + FirstPosition);
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
             Console.WriteLine("The number player got through dice is " + DiceRoll); 
+            Random random2 = new Random();
+            int Play=random2.Next(1, 4);
+            if (Play==1)
+            {
+                Console.WriteLine("Player got no play");
+                DiceRoll=0;
+                position = DiceRoll;
+                Console.WriteLine("The player position is: " + position);
+            }
+            else if (Play==2)
+            {
+                Console.WriteLine("Player got ladder");
+                position=(position+DiceRoll);
+                Console.WriteLine("The player position is: " + position);
+
+            }
+            else 
+            {
+                Console.WriteLine("Player got snake");
+                position=(position-DiceRoll);
+                if (position<0) 
+                {
+                   position=FirstPosition;
+                }
+                Console.WriteLine("The player position is: " + position);
+            }
+
             Console.ReadLine();
         }
     }
