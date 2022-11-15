6a344040304d522927de3375dc9e5a528f3327f9
dericklopis <stanforever08@gmail.com>
2022-11-15 21:46:40 +0530

    Added: Run till exact 100

--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -12,12 +12,12 @@ namespace SnakesAndLadders
         {
             const int FirstPosition =0;
             int position = 0;
-            
+            int DiceRoll;
             Console.WriteLine("First player is starting with position " + FirstPosition);
             while(position < 100)
             {
                 Random random = new Random();
-                int DiceRoll = random.Next(1, 7);
+                DiceRoll = random.Next(1, 7);
                 Console.WriteLine("The number player got through dice is " + DiceRoll);
                 Random random2 = new Random();
                 int Play = random2.Next(1, 4);
@@ -26,13 +26,11 @@ namespace SnakesAndLadders
                     Console.WriteLine("Player got no play");
                     DiceRoll=0;
                     position =position + DiceRoll;
-                    Console.WriteLine("The player position is: " + position);
                 }
                 else if (Play==2)
                 {
                     Console.WriteLine("Player got ladder");
                     position=(position+DiceRoll);
-                    Console.WriteLine("The player position is: " + position);
 
                 }
                 else
@@ -42,11 +40,14 @@ namespace SnakesAndLadders
                     if (position<0)
                     {
                         position=FirstPosition;
-                    }
-                    Console.WriteLine("The player position is: " + position);
+                    }                    
                 }
+                if (position>100)
+                {
+                    position=position-DiceRoll;
+                }
+                Console.WriteLine("The player position is: " + position);
             }
-           
 
             Console.ReadLine();
         }
