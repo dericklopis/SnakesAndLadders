c3ea694d6405a043dfbc1741babd5f8f77e57571
dericklopis <stanforever08@gmail.com>
2022-11-15 21:25:54 +0530

    Added: Repeat till player reaches 100

--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -14,37 +14,42 @@ namespace SnakesAndLadders
             int position = 0;
             
             Console.WriteLine("First player is starting with position " + FirstPosition);
-            Random random = new Random();
-            int DiceRoll = random.Next(1, 7);
-            Console.WriteLine("The number player got through dice is " + DiceRoll); 
-            Random random2 = new Random();
-            int Play=random2.Next(1, 4);
-            if (Play==1)
+            while(position < 100)
             {
-                Console.WriteLine("Player got no play");
-                DiceRoll=0;
-                position = DiceRoll;
-                Console.WriteLine("The player position is: " + position);
-            }
-            else if (Play==2)
-            {
-                Console.WriteLine("Player got ladder");
-                position=(position+DiceRoll);
-                Console.WriteLine("The player position is: " + position);
-
-            }
-            else 
-            {
-                Console.WriteLine("Player got snake");
-                position=(position-DiceRoll);
-                if (position<0) 
+                Random random = new Random();
+                int DiceRoll = random.Next(1, 7);
+                Console.WriteLine("The number player got through dice is " + DiceRoll);
+                Random random2 = new Random();
+                int Play = random2.Next(1, 4);
+                if (Play==1)
                 {
-                   position=FirstPosition;
+                    Console.WriteLine("Player got no play");
+                    DiceRoll=0;
+                    position =position + DiceRoll;
+                    Console.WriteLine("The player position is: " + position);
                 }
-                Console.WriteLine("The player position is: " + position);
+                else if (Play==2)
+                {
+                    Console.WriteLine("Player got ladder");
+                    position=(position+DiceRoll);
+                    Console.WriteLine("The player position is: " + position);
+
+                }
+                else
+                {
+                    Console.WriteLine("Player got snake");
+                    position=(position-DiceRoll);
+                    if (position<0)
+                    {
+                        position=FirstPosition;
+                    }
+                    Console.WriteLine("The player position is: " + position);
+                }
             }
+           
 
             Console.ReadLine();
         }
     }
 }
+    
